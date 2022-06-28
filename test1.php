e685a32026f548df05d6fbe0833d3ffde39856f1
BilalAss <85964491+BilalAss@users.noreply.github.com>
2022-06-28 17:21:59 +0200

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,176 @@
+<?php
+
+// $1num=3;
+// echo $1num;
+
+// $num1 =12;
+// $num2=12;
+// $num3="12";
+
+// echo $num3 == $num1; 
+// echo "<br>";
+// echo $num3 === $num1;
+// echo "<br>";
+// echo $num3 != $num1;
+// echo "<br>";
+// echo $num3 !==$num1;
+// echo "<br>";
+
+
+// $num11=12;
+// $num22=12;
+// $num33=33;
+// if ($num11==$num22 or $num11==$num33){
+//      echo "das is falsch";}
+// if ($num11==$num22 and $num11==$num33){
+//      echo "das is falsch";}
+
+// $studenten=array("Bilal","Rama","Hussien");
+// echo count($studenten);
+// echo"<br>";
+// $studenten[3]="Halla";
+// echo $studenten[3];
+
+// $student= array(
+//     array("Bilal","Rama","Halla"),
+//     array(2,3,2),
+//     array(33,26,35),   
+// );
+// echo $student[0][1];
+// echo"<br>";
+// $student[0][1]="Bilal2";
+// echo"<br>";
+// echo $student[0][1];
+// echo"<br>";
+// $student[0][3]="Rama";
+// echo $student[0][3];
+
+// $studennote1=12;
+// $studennote2=14;
+
+// if ($studennote1 >$studennote2){
+//     echo "student1 ist besser als student2";
+// }
+// elseif ($studennote1<$studennote2){
+//     echo "student2 ist besser als student1";
+// }
+// else {
+//     echo "student1 ist gut wie student2 ";
+// }
+
+// $student= array(
+//     array("Bilal","Rama","Halla"),
+//     array(33,26,35),   
+// );
+
+// for ($i=0;$i<3;$i++)
+// {
+//     echo $student[1][$i];
+//     echo "<br>";
+// }
+// echo "<br>";
+// $i=0;
+// while($i<3)
+// {
+//     echo $student[1][$i]."<br>";
+//     $i++;
+// }
+
+
+// function sum1($num1, $num2){
+//     echo $num1 + $num2;
+// }
+
+
+// sum1(3,2);
+// echo "heute";
+
+// $var1="dd";
+// $var2="rr";
+// if($var1=="Alex" or $var2=="Alex"){
+//     echo "Hallo Alex"."<br>";
+// }
+// elseif( $var1=="Lukas"or $var2=="Lukas"){
+//     echo "Hallo Lukas"."<br>";
+// }
+
+
+// else {
+//     echo"Keine der Variablen den Wert lukas oder lukas"."<br>";
+// }
+
+
+// $var1="10";
+// $var2=10;
+// if($var1!==$var2){
+//     print"Hallo";}
+//     else {
+//         echo"bey";
+//     }
+
+
+// $var=array("hh"=>4,"bb"=>(5.3));
+// echo $var["bb"];
+
+
+
+// for ($i=10;$i>0;$i--){
+//     echo $i ."<br>";
+// }
+// echo "<br>";
+// $j=10;
+// while($j>0){
+//     echo $j. "<br>";
+//     $j--;
+// }
+
+// $sum=0;
+// for ($i=10;$i>0;$i--){
+//     $sum+=$i;   
+// }
+// echo $sum ."<br>";
+
+
+// echo "<br>";
+// $sum=0;
+// $j=10;
+// while($j>0){
+//     $sum+=$j;
+//     $j--;
+// }
+// echo $sum. "<br>";
+
+
+// function addFun($Geh1,$Geh2,$Geh3){
+//     echo "The Result ist: ".$Geh1+$Geh2+$Geh3;
+// }
+// addfun(2200,3000,2700);
+
+// $num1=3;
+// $num2="2";
+
+// if ($num1 <$num2){
+//     echo "true";}
+//     else{
+//         echo "false";
+    
+
+// }
+ 
+// for ($i=1;$i<=4;$i++){
+// echo $i**2 . "<br>";
+// }
+
+
+
+// $student=array ("Bilal"=>3,"Ahamad"=>2);
+// echo $student["Bilal"];
+
+// $a =0;
+// while($a<5){
+//     echo $a  ."<br>";
+//      $a++;
+//     }
+
+
+?>
